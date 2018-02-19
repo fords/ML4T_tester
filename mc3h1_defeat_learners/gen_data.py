4a4a1afee906a765ac3c0787ed6c64c3b12565b4
fords <fords@users.noreply.github.com>
2018-02-19 06:38:20 -0500

    change

--- mc3h1_defeat_learners/gen_data.py
+++ mc3h1_defeat_learners/gen_data.py
@@ -9,13 +9,13 @@ import math
 # better for linear regression than decision trees
 def best4LinReg(seed=1489683273):
     np.random.seed(seed)
-    X = np.zeros((100,2))
-    Y = np.random.random(size = (100,))*200-100
+    #X = np.zeros((100,2))
+    #Y = np.random.random(size = (100,))*200-100
     # Here's is an example of creating a Y from randomly generated
     # X with multiple columns
     # Y = X[:,0] + np.sin(X[:,1]) + X[:,2]**2 + X[:,3]**3
-    row_number = np.random.randint(20,500)
-    col_number = np.random.randint(2,500)
+    row_number = np.random.randint(20,1001)
+    col_number = np.random.randint(2,1001)
     Y = np.zeros(row_number)
     X = np.random.normal(size = (row_number, col_number))
     for i in range(0,row_number):
@@ -26,9 +26,9 @@ def best4DT(seed=1489683273):
     np.random.seed(seed)
     #X = np.zeros((100,2))
     #Y = np.random.random(size = (100,))*200-100
-    X = np.random.normal(size = (np.random.randint(20,500),np.random.randint(2,500) ))
-    Y = np.zeros(np.random.randint(20,500))
-    for j in range(0,np.random.randint(2,500)):
+    X = np.random.normal(size = (np.random.randint(20,1001),np.random.randint(2,1001) ))
+    Y = np.zeros(np.random.randint(20,1001))
+    for j in range(0,np.random.randint(2,1001)):
         Y = Y + X[:,j] **2
     return X, Y
 
