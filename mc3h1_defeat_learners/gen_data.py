10518565fb3fbe5e52e593ffea616d687cd93866
fords <fords@users.noreply.github.com>
2018-02-19 06:42:19 -0500

    Fix

--- mc3h1_defeat_learners/gen_data.py
+++ mc3h1_defeat_learners/gen_data.py
@@ -14,23 +14,26 @@ def best4LinReg(seed=1489683273):
     # Here's is an example of creating a Y from randomly generated
     # X with multiple columns
     # Y = X[:,0] + np.sin(X[:,1]) + X[:,2]**2 + X[:,3]**3
-    row_number = np.random.randint(20,1001)
-    col_number = np.random.randint(2,1001)
-    Y = np.zeros(row_number)
-    X = np.random.normal(size = (row_number, col_number))
-    for i in range(0,row_number):
-        Y = Y + X[:,i]
+    rows = np.random.randint(10, 1001)
+    cols = np.random.randint(2, 1001)
+    X = np.random.normal(size=(rows, cols))
+    Y = np.zeros(rows)
+    for j in range(cols):
+        Y += X[:, j]
     return X, Y
 
 def best4DT(seed=1489683273):
     np.random.seed(seed)
     #X = np.zeros((100,2))
     #Y = np.random.random(size = (100,))*200-100
-    X = np.random.normal(size = (np.random.randint(20,1001),np.random.randint(2,1001) ))
-    Y = np.zeros(np.random.randint(20,1001))
-    for j in range(0,np.random.randint(2,1001)):
-        Y = Y + X[:,j] **2
+    rows = np.random.randint(10, 1001)
+    cols = np.random.randint(2, 1001)
+    X = np.random.normal(size=(rows, cols))
+    Y = np.zeros(rows)
+    for j in range(cols):
+        Y += X[:, j] ** 2
     return X, Y
+
 
 def author():
     return 'zwin3' #Change this to your user ID
