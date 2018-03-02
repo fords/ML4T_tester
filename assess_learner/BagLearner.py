d40b2c5c0bc2ab6208625efb6c3108f4261eb9b3
fords <fords@users.noreply.github.com>
2018-03-02 12:35:16 -0500

    Add

---
+++ assess_learner/BagLearner.py
@@ -0,0 +1,94 @@
+import numpy as np
+import LinRegLearner, DTLearner, RTLearner
+
+
+class BagLearner(object):
+
+    def __init__(self, learner, bags=20, boost=False, verbose=False, **kwargs):
+        """Initalize a Bag Learner
+        Parameters:
+        learner: A LinRegLearner, DTLearner, or RTLearner
+        bags: The number of learners to be trained using Bootstrap Aggregation
+        boost: If true, boosting will be implemented
+        verbose: If True, information about the learner will be printed out
+        kwargs: Keyword arguments to be passed on to the learner's constructor
+
+        Returns: An instance of Bag Learner
+        """
+        self.verbose = verbose
+        learners = []
+        for i in range(bags):
+            learners.append(learner(**kwargs))
+        self.learners = learners
+        self.kwargs = kwargs
+        self.bags = bags
+        self.boost = boost
+        if verbose:
+            self.get_learner_info()
+
+
+    def addEvidence(self, dataX, dataY):
+        """Add training data to learner
+        Parameters:
+        dataX: A numpy ndarray of X values to add
+        dataY: A numpy 1D array of Y values to add
+        Returns: Updated individual learners in BagLearner
+        """
+        # Sample the data with replacement
+        num_samples = dataX.shape[0]
+        for learner in self.learners:
+            idx = np.random.choice(num_samples, num_samples)
+            bagX = dataX[idx]
+            bagY = dataY[idx]
+            learner.addEvidence(bagX, bagY)
+        if self.verbose:
+            self.get_learner_info()
+
+
+    def query(self, points):
+        """Estimates a set of test points given the model we built
+
+        Parameters:
+        points: A numpy ndarray of test queries
+        Returns:
+        preds: A numpy 1D array of the estimated values
+        """
+        preds = np.array([learner.query(points) for learner in self.learners])
+        return np.mean(preds, axis=0)
+
+
+    def get_learner_info(self):
+        """Print out data for this BagLearner"""
+        learner_name = str(type(self.learners[0]))[8:-2]
+        print ("This BagLearner is made up of {} {}:".
+            format(self.bags, learner_name))
+
+        print ("kwargs =", self.kwargs)
+        print ("boost =", self.boost)
+
+        # Print out information for each learner within BagLearner
+        for i in range(1, self.bags + 1):
+            print (learner_name, "#{}:".format(i));
+            self.learners[i-1].get_learner_info()
+
+
+if __name__=="__main__":
+    print ("This is a Bag Learner\n")
+
+    # Some data to test the BagLearner
+    x0 = np.array([0.885, 0.725, 0.560, 0.735, 0.610, 0.260, 0.500, 0.320])
+    x1 = np.array([0.330, 0.390, 0.500, 0.570, 0.630, 0.630, 0.680, 0.780])
+    x2 = np.array([9.100, 10.900, 9.400, 9.800, 8.400, 11.800, 10.500, 10.000])
+    x = np.array([x0, x1, x2]).T
+
+    y = np.array([4.000, 5.000, 6.000, 5.000, 3.000, 8.000, 7.000, 6.000])
+
+    # Create a BagLearner from given training x and y
+    bag_learner = BagLearner(DTLearner.DTLearner, verbose=True)
+
+    print ("\nAdd data")
+    bag_learner.addEvidence(x, y)
+
+    # Query with dummy data
+    print ("Query with dummy data:\n", np.array([[1, 2, 3], [0.2, 12, 12]]))
+    print ("Query results:", bag_learner.query(np.array([[1, 2, 3], [0.2, 12, 12]])))
---
+++ assess_learner/RTLearner.py
@@ -0,0 +1,74 @@
+import numpy as np
+import pandas as pd
+from random import randint
+#
+
+class RTLearner(object):
+
+    def __init__(self, leaf_size, verbose=False, tree=None):
+        self.leaf_size = leaf_size
+        self.verbose = verbose
+        self.tree = tree
+
+        if verbose:
+            print ("leaf_size is :  ", self.leaf_size)
+            print (" Building tree not a forest...... \n")
+            #print np.shape(self.tree)
+        else:
+            pass
+
+    def author():
+        return "zwin3"
+
+    def get_indx(self, x_train, numberOfSamples):
+        leaf_indx =  np.random.randint(x_train.shape[1], size=1) # random indx for splitting randint(0, x_train.shape[1] -1 )
+        split_val = np.mean(x_train[:,leaf_indx])
+        return leaf_indx[0]
+
+    def make_DT_not_forest(self, Dataset ):
+        x_train = Dataset[:, 0:-1]
+        y_train = Dataset[:, -1:]
+        numberOfSamples = x_train.shape[0]
+        number_features = x_train.shape[1]
+        length =  np.random.randint(x_train.shape[1])
+        if ( len(y_train) == 1 ):   # labels are the same -> Base recursion
+            split_val = np.mean(y_train[:,])
+            return np.vstack(( self.tree ,  np.array( [['leaf' , split_val,  np.nan,  np.nan]] )))
+        elif  (x_train.shape[0] <= self.leaf_size) :   # Base recursion
+            split_val = np.mean(y_train[:,])
+            return np.vstack(( self.tree , np.array(  [['leaf', split_val, np.nan, np.nan]] )))
+        else:
+            split_val = np.median(Dataset[:, length])
+            right_tree_shape = ( Dataset[Dataset[:, length] > split_val]).shape[0]
+            if (right_tree_shape == 0):   # The end of tree
+                split_val = np.mean(Dataset[:, length])
+                leaf_val = y_train.mean()
+                return np.vstack((self.tree, np.array([['leaf', leaf_val, np.nan, np.nan]])))
+            left = Dataset[:, length] <= split_val
+            right = ~left  # complement of left
+            leftTree = self.make_DT_not_forest(Dataset[left])
+            rightTree = self.make_DT_not_forest(Dataset[right])
+            root = np.array([[length, split_val, 1, leftTree.shape[0] + 1]])
+            return np.vstack((self.tree, root, leftTree, rightTree))
+
+    def addEvidence(self, Xtrain, Ytrain):
+        self.tree = np.array([]).reshape((0,4))
+        Dataset = np.column_stack((Xtrain, Ytrain))
+        #Building the model using recursive calls
+        self.tree = self.make_DT_not_forest(Dataset)
+        if self.verbose:
+            print ("Tree shape is :  ", np.shape(self.tree))
+
+    def query(self, Xtest):
+        result = np.empty( [Xtest.shape[0], 1] , dtype = float)
+        for indx, row in enumerate(Xtest):
+            init_index = 0
+            temp_leaf = self.tree[ init_index , 0]
+            while ( temp_leaf != 'leaf'):
+                if (  row[int(float(temp_leaf) ) ] <= float( self.tree[init_index , 1] ) ):   # left side add indx + 1
+                    init_index += 1
+                else:                   # right side add indx + left side
+                    init_index += int( float( self.tree[init_index , 3] ) )
+                temp_leaf = self.tree[init_index , 0]
+            result[indx, 0] = self.tree[init_index , 1]
+        return result.flatten()
