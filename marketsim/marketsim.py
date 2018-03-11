e061a01cb00323d7606ac4e52b5fe52894b49535
fords <fords@users.noreply.github.com>
2018-03-11 03:25:04 -0400

    comment out test cases

--- marketsim/marketsim.py
+++ marketsim/marketsim.py
@@ -158,11 +158,11 @@ def test_code():
     cum_ret_SPY, avg_daily_ret_SPY, std_daily_ret_SPY, sharpe_ratio_SPY = assess_portfolio(portvals_SPX,sf =252)
     #cum_ret_SPY, avg_daily_ret_SPY, std_daily_ret_SPY, sharpe_ratio_SPY = [0.2,0.01,0.02,1.5]
 
-    daily_returns = portvals[1:].values / portvals[:-1] - 1
-    cum_ret = portvals[-1] / portvals[0] - 1
-    avg_daily_ret = daily_returns.mean()
-    std_daily_ret = daily_returns.std()
-    sharpe_ratio = np.sqrt(245) * avg_daily_ret / std_daily_ret  #sf = 245
+    #daily_returns = portvals[1:].values / portvals[:-1] - 1
+    #cum_ret = portvals[-1] / portvals[0] - 1
+    #avg_daily_ret = daily_returns.mean()
+    #std_daily_ret = daily_returns.std()
+    #sharpe_ratio = np.sqrt(245) * avg_daily_ret / std_daily_ret  #sf = 245
 
     # Compare portfolio against $SPX
     print "Date Range: {} to {}".format(start_date, end_date)
